ca20fc66f1b6d2a266cb7c68fa0b2af5211553bf
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-02-06 12:11:23 +0000

    Added methods for SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    term: "",
+  };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.term);
+
+    // TODO make sure we call callback from parent comp
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
