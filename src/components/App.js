de0a4fe35faa88d49782f3f41c54c9b302e4e4a0
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-02-07 21:17:29 +0000

    Fixed warnings and finished App practice.

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,20 @@ class App extends React.Component {
     selectedVideo: null,
   };
 
+  componentDidMount() {
+    this.onTermSumbit("buildings");
+  }
+
   onTermSumbit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
