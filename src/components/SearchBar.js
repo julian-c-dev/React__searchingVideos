1aeb4ade4320975a074d0cc3eefebdd226be1c3d
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-03-28 18:09:12 +0100

    SearchBar comp transform class to functional comp with Hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = {
-    term: "",
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onInputChange = (e) => {
+    setTerm(e.target.value);
   };
 
-  onInputChange = (e) => {
-    this.setState({ term: e.target.value });
-  };
-
-  onFormSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-
-    console.log("onFormSubmit in SearchBar:", this.state.term);
-
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={onInputChange} />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
